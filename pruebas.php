7f8a978a538e5e8aeeac074a2ea89c4f3fdd1c6b
martaortimorales <62881192+martaortimorales@users.noreply.github.com>
2020-04-02 09:57:00 +0200

    Subido fichero pruebas php

---
+++ pruebas.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="description" content="Proyecto de prueba de php y bd">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<title>Curso GitHub</title>
+</head>
+<body class="container-fluid">
+	<h1>Curso OOP-SQL <small>Programa para tiendas</small></h1>
+
+	<?php
+		echo "Prueba de php para github";
+	?>
+
+</body>
+</html>
